100482c192d3d13975d8a28cc61504d52458fa5f
hermkan <hermann.kanga.dev@gmail.com>
2022-09-14 11:19:22 -0400

    rebuild v15

--- server/src/app.js
+++ server/src/app.js
@@ -9,10 +9,10 @@ const app = express();
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join('public', 'index.html')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', api);
 app.get('/*', (req, res) => {
-  res.sendFile(path.join('public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
-console.log(path.join('public', 'index.html'));
+console.log(path.join(__dirname, 'public', 'index.html'));
 module.exports = app;
